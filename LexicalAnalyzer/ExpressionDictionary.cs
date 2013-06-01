598beae29f23649b9b82bc417e6b03904044486f
Maikel Hofman <mh.carducci@gmail.com>
2013-06-01 19:09:34 +0200

    Added ExpressionDictionary that holds the regular expressions

---
+++ LexicalAnalyzer/ExpressionDictionary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using LexicalAnalyzer.Token;
+
+namespace LexicalAnalyzer
+{
+    public class ExpressionDictionary : Dictionary<Type, String>
+    {
+        public ExpressionDictionary()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            Add(typeof(Float), "");
+            Add(typeof(Integer), "^(?<=\\s|^)\\d+(?=\\s|$|[^.e])");
+        }
+    }
+}

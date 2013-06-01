82227832a75ebb2c9fba4d4f2570aa3ec3f2a45c
Maikel Hofman <mh.carducci@gmail.com>
2013-06-01 19:09:09 +0200

    Added different Tokens

---
+++ LexicalAnalyzer/Token/Integer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexicalAnalyzer.Token
+{
+    public class Integer : Number
+    {
+        public Integer(String Value) : base(Value) { }
+    }
+}

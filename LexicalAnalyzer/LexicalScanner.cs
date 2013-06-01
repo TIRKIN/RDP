3f4a8d1c3ac57ed41a0beedc01827aaaa586fce6
Maikel Hofman <mh.carducci@gmail.com>
2013-06-01 15:26:56 +0200

    Added basic interface for LexicalAnalyzer

---
+++ LexicalAnalyzer/LexicalScanner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LexicalAnalyzer.Token;
+
+namespace LexicalAnalyzer
+{
+    public class LexicalScanner
+    {
+        private string _input;
+
+        public LexicalScanner(string Input)
+        {
+            _input = Input;
+        }
+
+        public AbstractToken GetNextToken()
+        {
+
+        }
+    }
+}
---
+++ LexicalAnalyzer/Token/Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexicalAnalyzer.Token
+{
+    public class Number : AbstractToken
+    {
+        public Number(string Value)
+        {
+            this._value = Value;
+        }
+    }
+}

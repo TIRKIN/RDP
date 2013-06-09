b9a5f1c19dedff486a8acc3c2169025802bae8b2
Maikel Hofman <mh.carducci@gmail.com>
2013-06-09 15:45:38 +0200

    Added support for equal sign in lexical analyzer

---
+++ LexicalAnalyzer.Test/TokenTest/EqualsTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using LexicalAnalyzer.Token;
+using NUnit.Framework;
+
+namespace LexicalAnalyzer.Test.TokenTest
+{
+    [TestFixture]
+    class EqualsTest
+    {
+        private ExpressionDictionary _dictionary;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dictionary = new ExpressionDictionary();
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.AreEqual("=", Regex.Match("=", _dictionary[typeof(Equals)]).Value);    
+        }
+    }
+}
--- LexicalAnalyzer/ExpressionDictionary.cs
+++ LexicalAnalyzer/ExpressionDictionary.cs
@@ -16,10 +16,11 @@ namespace LexicalAnalyzer
             Add(typeof(Float), "^\\s*([0-9][0-9]*)?(\\.[0-9]+)+([eE][+-]?[0-9]+)?");
             Add(typeof(Integer), "^\\s*(?<=\\s|^)\\d+(?=\\s|$|[^.e])");
             Add(typeof(AddSub), "^\\s*[+|-]");
-            Add(typeof(Operator), "^\\s*[*|\\|^]");
+            Add(typeof(Operator), "^\\s*[*|\\\\|^]");
             Add(typeof(OpenParenthesis), "^\\s*\\(");
             Add(typeof(CloseParenthesis), "^\\s*\\)");
             Add(typeof(Variable), "^\\s*[a-z]");
+            Add(typeof(Equals), "^\\s*=");
         }
     }
 }

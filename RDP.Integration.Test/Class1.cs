d102c3ce4da2bdbbbade43f10fdc176c08b0187c
Maikel H <mh.carducci@gmail.com>
2013-06-25 18:59:01 +0200

    Added custom tree in semantic analyzer test

--- RDP.Integration.Test/Class1.cs
+++ RDP.Integration.Test/Class1.cs
@@ -11,6 +11,35 @@ namespace RDP.Integration.Test
     [TestFixture]
     public class IntegrationTest
     {
+        private ParseNode _testTree;
+
+        [SetUp]
+        public void Setup()
+        {
+            _testTree = new ParseNode(ParseEnum.Start);
+            _testTree.AddChild(new ParseNode(ParseEnum.Expression));
+            _testTree.getChildren()[0].AddChild(new ParseNode(ParseEnum.Term));
+            _testTree.getChildren()[0].AddChild(new ParseNode(ParseEnum.ExpressionAccent));
+            ParseNode node = _testTree.getChildren()[0].getChildren()[0];
+            node.AddChild(new ParseNode(ParseEnum.Factor));
+            node.AddChild(new ParseNode(ParseEnum.TermAccent));
+            node.getChildren()[0].AddChild(new ParseNode(ParseEnum.Number, "3"));
+            node.getChildren()[1].AddChild(new ParseNode(ParseEnum.Empty));
+
+            node = _testTree.getChildren()[0].getChildren()[1];
+            node.AddChild(new ParseNode(ParseEnum.Operator, "+"));
+            node.AddChild(new ParseNode(ParseEnum.Term));
+            node.AddChild(new ParseNode(ParseEnum.ExpressionAccent));
+            ParseNode termNode = node.getChildren()[1];
+            
+            termNode.AddChild(new ParseNode(ParseEnum.Factor));
+            termNode.AddChild(new ParseNode(ParseEnum.TermAccent));
+            termNode.getChildren()[0].AddChild(new ParseNode(ParseEnum.Number, "4"));
+            termNode.getChildren()[1].AddChild(new ParseNode(ParseEnum.Empty));
+
+            node.getChildren()[2].AddChild(new ParseNode(ParseEnum.Empty));    
+        }
+
         [Test]
         public void SimpleExpressionTest()
         {
@@ -20,7 +49,7 @@ namespace RDP.Integration.Test
             ParseNode parseTree = parser.parse();
 
             var analyzer = new SemanticAnalyzer.SemanticAnalyzer();
-            ASTNode AST = analyzer.GenerateAST(parseTree);
+            ASTNode AST = analyzer.GenerateAST(_testTree);
 
             
         }

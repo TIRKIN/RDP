602d72bdb98dbdc864752b6105437e0657bea845
Maikel Hofman <mh.carducci@gmail.com>
2013-06-25 18:45:26 +0200

    Added integration test

---
+++ RDP.Integration.Test/Class1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using SemanticAnalyzer.SyntaxTree;
+using SemanticAnalyzer.SyntaxTree.Node;
+using recursive_descent_parser;
+using recursive_descent_parser.ParseTree;
+using SemanticAnalyzer;
+
+
+namespace RDP.Integration.Test
+{
+    [TestFixture]
+    public class IntegrationTest
+    {
+        [Test]
+        public void SimpleExpressionTest()
+        {
+            string test = "3+4";
+            
+            Parser parser = new Parser(test);
+            ParseNode parseTree = parser.parse();
+
+            var analyzer = new SemanticAnalyzer.SemanticAnalyzer();
+            ASTNode AST = analyzer.GenerateAST(parseTree);
+
+            
+        }
+    }
+}
